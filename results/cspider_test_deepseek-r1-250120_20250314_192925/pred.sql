cb7909d084d0b5ce3e8f74bbdb8f2c910e5486dc
ZQ <zqzqsb@gmail.com>
2025-03-19 16:35:09 +0800

    fix: clear test results

--- results/cspider_test_deepseek-r1-250120_20250314_192925/pred.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) FROM singer;
-SELECT COUNT(*) FROM singer;
-SELECT Name, Country, Age FROM singer ORDER BY Age DESC;
-SELECT Name, Country, Age FROM singer ORDER BY Age DESC;
-SELECT AVG(Age) AS Average_Age, MIN(Age) AS Min_Age, MAX(Age) AS Max_Age FROM singer WHERE Country = 'France';
